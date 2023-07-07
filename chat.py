a3da9015982865c485e41206a97b10ad3097a8e6
Kai Xu <me@xuk.ai>
2023-07-07 15:34:29 -0400

    chore: update price

--- chat.py
+++ chat.py
@@ -51,9 +51,10 @@ CONFIG_FILEPATHS = [
 ]
 
 PRICING_RATE = {
-    "gpt-3.5-turbo": {"prompt": 0.002, "completion": 0.002},
-    "gpt-4":         {"prompt": 0.03,  "completion": 0.06},
-    "gpt-4-32k":     {"prompt": 0.06,  "completion": 0.12},
+    "gpt-3.5-turbo":     {"prompt": 0.0015, "completion": 0.002},
+    "gpt-3.5-turbo-16k": {"prompt": 0.003,  "completion": 0.004},
+    "gpt-4":             {"prompt": 0.03,   "completion": 0.06},
+    "gpt-4-32k":         {"prompt": 0.06,   "completion": 0.12},
 }
 
 PROMPT_PREFIX = ">>> "
