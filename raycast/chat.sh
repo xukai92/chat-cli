f8aac0c7e3e1ed763f8db0308ecf81a688bc53f2
Kai Xu <me@xuk.ai>
2023-08-28 10:51:07 -0400

    feat: spawn wezterm in same session (default)

--- raycast/chat.sh
+++ raycast/chat.sh
@@ -15,4 +15,7 @@
 # @raycast.author Kai Xu
 # @raycast.authorURL xuk.ai
 
-wezterm --config "font_size=20" start $HOME/bin/chat "$1"
+# Option 1: Creating a new icon in the dock
+#wezterm --config "font_size=20" start $HOME/bin/chat "$1"
+# Option 2: Reusing the exisiting icon in the dock
+WEZTERM_UNIX_SOCKET=~/.local/share/wezterm/default-org.wezfurlong.wezterm wezterm cli spawn --new-window $HOME/bin/chat "$1"
